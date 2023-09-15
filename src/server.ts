0364c1bc4456b275ff7deb7b0a4ef1e522df49c2
angela-oliveira <angela.oliveira@stage.consulting>
2023-09-14 22:51:23 -0300

    fix: import

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { getAllPromptRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
 import { generateAICompletionRoute } from "./routes/generate-ai-completion";
-import fastifyCors from "@fastify/cors";
+import { fastifyCors } from "@fastify/cors";
 
 const app = fastify()
 
